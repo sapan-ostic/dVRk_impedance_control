08e81aa3ff1e03f59efa0a68c37b5cd5930ab5b8
sapan-ostic <sapanagrawal777@gmail.com>
2019-11-27 16:08:20 -0500

    Impedance controller with linear trajGenerator

--- Impedance_controller/main.py
+++ Impedance_controller/main.py
@@ -30,9 +30,10 @@ class KukaController:
 
 		self.NJoints = get_joint_num()
 		self.cmd_msg = cmd_msg
-		self.prev_time = rospy.get_time()
-		
-		## CTC Joint Space
+		self.t0 = rospy.get_time()
+		self.prev_time = 0
+
+		## Joint Space
 		self.prev_state = np.zeros(self.NJoints)
 		self.prev_vel = np.zeros(self.NJoints)
 		self.prev_vel1 = np.zeros(self.NJoints)
@@ -41,14 +42,97 @@ class KukaController:
 		self.prev_vel4 = np.zeros(self.NJoints)
 		self.prev_wt_vel = np.zeros(self.NJoints)
 
-		## CTC Task Space
+		## Task Space
 		self.prev_x = np.zeros(3)
 		self.prev_velX = np.zeros(3)
-		self.prev_J = get_end_effector_jacobian(self.prev_state) 
+
+		# Set end-effector desired velocity here
+		self.X_SPEED = np.zeros(3)
+		self.X_SPEED[0] = 1 
+		
+		self.x_speed_local = self.X_SPEED 
 
 	def getJointState(self, data):
 		self.state = np.asarray(data.joint_positions) 
-		self.Task_impedance_control()
+		self.t = rospy.get_time() - self.t0 #clock
+		self.Task_impedance_control() 
+
+	def generate_trajectory(self, x):
+		xllim = -0.6
+		xulim = 0.6
+
+		if x[0] > xulim :
+			self.x_speed_local = -self.X_SPEED
+		elif x[0] < xllim :
+			self.x_speed_local = self.X_SPEED
+
+		x_des = x + self.x_speed_local * (self.dt)	
+		velX_des = self.x_speed_local
+
+		print "x_speed_local : ", self.x_speed_local  
+		print "velX_des", velX_des
+
+		return [x_des, velX_des]
+
+	def Task_impedance_control(self):
+
+		Kp = 0.3*np.eye(3) #Stiffness Matrix
+		# Kp[1][1] = 0 
+		# Kp[2][2] = 0
+
+		Kd = 0.0001*np.eye(3) # Damping Matrix
+		Md = 0.02*np.eye(3)
+
+		self.dt = self.t - self.prev_time
+		x = get_end_effector_pos(self.state)
+		print "x = ", x
+
+		traj = self.generate_trajectory(x)
+
+		# stateGoal = np.array([-0.08,-1.5, 0.07, -0.9, -2.07, 2.2, -0.8])
+		XGoal = traj[0]
+		XGoal[1] = 0.3
+		XGoal[2] = 0.2
+		XvelGoal = traj[1]
+		XaccGoal = 0*np.ones(3)
+
+		# while loop stuff here
+
+	
+		vel = (self.state - self.prev_state)/self.dt
+		wt_vel = (0.925*vel + 0.7192*self.prev_vel + 0.4108*self.prev_vel1+0.09*self.prev_vel2)/(0.4108+0.7192+0.925+0.09)
+		acc = (wt_vel - self.prev_wt_vel)/self.dt
+
+		velX = (x - self.prev_x)/self.dt
+		
+		errX = np.asarray(XGoal - x)
+		derrX = XvelGoal - velX
+
+		J = get_end_effector_jacobian(self.state)
+		J_inv = np.linalg.pinv(J)
+
+		Mq = get_M(self.state)
+
+		Mx = np.dot(np.dot(np.transpose(J_inv),Mq),J_inv)
+
+		F = np.dot(np.dot(np.linalg.inv(Md), Mx),( np.dot(Kd, derrX) + np.dot(Kp, errX) ) )
+
+		G = get_G(self.state)
+		# G = inverse_dynamics(self.state, vel, acc)
+		tau = G + np.dot(np.transpose(J),F)
+
+		self.cmd_msg.joint_cmds = [tau[0],tau[1],tau[2],tau[3],tau[4],tau[5],tau[6]] 
+		self.pub.publish(self.cmd_msg)
+
+		self.prev_time = self.t    
+		self.prev_state = self.state
+		self.prev_vel = vel
+		self.prev_vel1 = self.prev_vel
+		self.prev_vel2 = self.prev_vel1
+		self.prev_vel3 = self.prev_vel2
+		self.prev_vel4 = self.prev_vel3
+		self.prev_wt_vel = wt_vel
+		self.prev_x = x			
 
 	def CTC_joint_controller(self):
 
@@ -94,67 +178,7 @@ class KukaController:
 		self.prev_vel4 = self.prev_vel3
 		self.prev_wt_vel = wt_vel
 
-	def Task_impedance_control(self):
 
-		Kp = 0.1*np.eye(3) #Stiffness Matrix
-		# Kp[1][1] = 0 
-		Kp[2][2] = 0
-
-		Kd = 0.0001*np.eye(3) # Damping Matrix
-		Md = 0.01*np.eye(3) 
-		
-
-		# Kp = 0.0*np.eye(3) #Stiffness Matrix
-		# Kd = 0.0000*np.eye(3) # Damping Matrix
-		# Md = 0.001*np.eye(3)
-
-		# stateGoal = np.array([-0.08,-1.5, 0.07, -0.9, -2.07, 2.2, -0.8])
-		XGoal = 0.3*np.ones(3) 
-		XvelGoal = 0*np.ones(3)
-		XaccGoal = 0*np.ones(3)
-
-		# while loop stuff here
-		time = rospy.get_time()
-		dt = time - self.prev_time  
-		# print(1/dt)      
-		self.prev_time = time    
-
-		x = get_end_effector_pos(self.state)
-		print "x = ", x
-	
-		vel = (self.state - self.prev_state)/dt
-		wt_vel = (0.925*vel + 0.7192*self.prev_vel + 0.4108*self.prev_vel1+0.09*self.prev_vel2)/(0.4108+0.7192+0.925+0.09)
-		acc = (wt_vel - self.prev_wt_vel)/dt
-
-		velX = (x - self.prev_x)/dt
-		
-		errX = np.asarray(XGoal - x)
-		derrX = XvelGoal - velX
-
-		J = get_end_effector_jacobian(self.state)
-		J_inv = np.linalg.pinv(J)
-
-		Mq = get_M(self.state)
-
-		Mx = np.dot(np.dot(np.transpose(J_inv),Mq),J_inv)
-
-		F = np.dot(np.dot(np.linalg.inv(Md), Mx),( np.dot(Kd, derrX) + np.dot(Kp, errX) ) )
-
-		G = get_G(self.state)
-		# G = inverse_dynamics(self.state, vel, acc)
-		tau = G + np.dot(np.transpose(J),F)
-
-		self.cmd_msg.joint_cmds = [tau[0],tau[1],tau[2],tau[3],tau[4],tau[5],tau[6]] 
-		self.pub.publish(self.cmd_msg)
-
-		self.prev_state = self.state
-		self.prev_vel = vel
-		self.prev_vel1 = self.prev_vel
-		self.prev_vel2 = self.prev_vel1
-		self.prev_vel3 = self.prev_vel2
-		self.prev_vel4 = self.prev_vel3
-		self.prev_wt_vel = wt_vel
-		self.prev_x = x	
 
 	def CTC_task_controller(self):
 
